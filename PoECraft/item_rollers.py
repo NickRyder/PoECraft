1ba5d35959b2f050717cb3f493fc8c6ce623a158
Nick Ryder <nick.ryder@berkeley.edu>
2019-11-17 12:16:51 -0800

    Refactored to include mod_collector module

--- PoECraft/item_rollers.py
+++ PoECraft/item_rollers.py
@@ -4,76 +4,8 @@ import numpy as np
 from RePoE import base_items, item_classes, essences, fossils, mods, mod_types
 import copy
 from collections import Counter
+from PoECraft.mod_collector import collect_mods_and_tags
 
-
-
-def _get_positive_spawn_weight_tags(affix, starting_tags) -> set:
-    '''
-    Takes in an affix dict and a set of starting_tags
-    Finds all positive weight tags in spawn_weights
-    Ignores all spawn_weights if one of our starting tags forces a spawn_weight of zero
-    '''
-    pos_spawn_weight_tags = set()
-    for spawn_weight in affix["spawn_weights"]:
-        if spawn_weight["weight"] == 0:
-            #We read the spawn_weights in order they are listed
-            #If we find one which gives matches a tag we have we in starting_tags, we ignore
-            if spawn_weight["tag"] in starting_tags:
-                break
-        elif spawn_weight["weight"] > 0:
-            pos_spawn_weight_tags.add(spawn_weight["tag"])
-        else:
-            raise ValueError("spawn_weights should be nonnegative")
-
-    return pos_spawn_weight_tags
-
-
-# TODO: MAYBE?: this should take in some generic item type and calculate the possible mods that 
-# can roll on that item. This will allow support for items which have some odd non-spawnable
-# mods on them (like league specific mod drops)
-# TODO: currently includes more tags than necessary (includes all possible relevant tags)
-def generate_all_possible_affixes_and_tags(starting_tags, mod_pool):
-    '''
-    :param starting_tags: set containing all the starting_tags on the item we are rolling
-    :param mod_pool: a dictionary containing all possible mods possible to roll on our item
-    :return: spawn_tags, affixes
-            spawn_tags -   all possible tags which can spawn from rolling this item
-            affixes -      final mod pool of all things that can spawn
-    '''
-
-    affixes = {}
-    added_new_affixes = True
-
-    while added_new_affixes:
-    
-        added_new_affixes = False
-
-        total_pos_spawn_weights_tags = set()
-
-        for key, affix in mod_pool.items():
-            # We want to only consider affixes which have a positive spawn weight for one of our potential tags
-            # We also want to keep track of which tags affect this affix, whether they show up as any spawn_weight (even 0)
-            # or they show up as a generation_weight
-
-            #important that we only block starting_tags here, as there could be routes of crafting which spawn some tags and not others which affects blocking
-            pos_spawn_weights_tags = _get_positive_spawn_weight_tags(affix, starting_tags)
-            total_pos_spawn_weights_tags = total_pos_spawn_weights_tags.union(pos_spawn_weights_tags)
-
-            spawn_weight_tags = set([spawn_weight["tag"] for spawn_weight in affix["spawn_weights"]])
-            assert pos_spawn_weights_tags.issubset(spawn_weight_tags), "pos_spawn_weight_tags should be subset of spawn_weight_tags"
-
-            add_tags = set()
-            if len(pos_spawn_weights_tags) > 0:
-                if key not in affixes:
-                    affixes[key] = affix
-                    added_new_affixes = True
-
-                add_tags = add_tags.union(set(affix["adds_tags"]))
-
-    spawn_tags = set(starting_tags.copy())
-    spawn_tags = spawn_tags.union(add_tags)    
-
-    return spawn_tags, affixes
 
 def get_mod_type_generation_weight_for_affix(affix, mod_type_tags):
     '''
@@ -123,25 +55,6 @@ def get_spawn_weighting(affix, tags, mod_type_tags):
 
     raise ValueError("spawn_weights did not contain appropriate tag for spawning")
 
-def _get_spawnable_mods_for_item(domains,ilvl):
-    '''
-    Trim by removing all affixes that can't be crafted and are in the wrong domains
-    '''
-
-    spawnable_mods = {}
-
-    for affix_key in list(mods.keys()):
-        affix_value = mods[affix_key]
-        is_prefix_or_suffix = affix_value["generation_type"] in ["prefix", "suffix"]
-        is_in_domains = affix_value["domain"] in domains
-        is_not_essence = not affix_value["is_essence_only"]
-        is_ilvl_appropriate = affix_value["required_level"] <= ilvl
-
-
-        if is_prefix_or_suffix and is_in_domains and is_not_essence and is_ilvl_appropriate:
-            spawnable_mods[affix_key] = affix_value
-    return spawnable_mods
-
 
 def tags_to_spawn_weights(tags: set, global_generation_weights, affix_data):
     spawn_weights = [get_spawn_weighting(affix_datum, tags, global_generation_weights) for affix_datum in affix_data]
@@ -187,6 +100,7 @@ def generate_prefix_suffix_lookups(affix_data):
             suffix_bits[index] = True
     return prefix_bits, suffix_bits
 
+#TODO:cleanup
 def generate_spawn_tag_lookup_tables(spawn_tags_to_spawn_weight: np.array, affix_data):
     affix_N = len(affix_data)
     tag_N = spawn_tags_to_spawn_weight.shape[0]
@@ -273,7 +187,7 @@ class hash_weight_dict():
 
 
 
-    def __init__(self, mods, starting_tags, domains = ['item'], global_generation_weights = [], added_mods = {}, is_sanctified = False, ilvl = 100):
+    def __init__(self, starting_tags, domains = ['item'], global_generation_weights = [], added_mods = {}, is_sanctified = False, ilvl = 100):
         '''
 
         '''
@@ -284,13 +198,7 @@ class hash_weight_dict():
 
         # self.is_sanctified = is_sanctified
 
-        spawnable_mods = _get_spawnable_mods_for_item(domains, ilvl)
-        mod_pool = {**spawnable_mods, **added_mods}
-
-        #further reduce the mod pool by looking at spawn weights and tags
-        realized_spawn_tags, mod_pool = generate_all_possible_affixes_and_tags(starting_tags, mod_pool)
-
-        self.base_dict = {**mod_pool, **added_mods}
+        self.base_dict, realized_spawn_tags = collect_mods_and_tags(domains=domains, starting_tags=starting_tags, added_mods=added_mods, ilvl=ilvl)
 
         ##Order the keys and data
         self.affix_keys = list(self.base_dict.keys())
---
+++ PoECraft/mod_collector.py
@@ -0,0 +1,103 @@
+from RePoE import mods
+
+'''This module is intended to take in a base item and construct a list of all possible mods that can roll on that base item
+    through currency crafting in PoE. Excludes not spawnable mods like league specific mods and master-crafts'''
+
+
+def _get_positive_spawn_weight_tags(affix, starting_tags) -> set:
+    '''
+    Takes in an affix dict and a set of starting_tags
+    Finds all positive weight tags in spawn_weights
+    Ignores all spawn_weights if one of our starting tags forces a spawn_weight of zero
+    '''
+    pos_spawn_weight_tags = set()
+    for spawn_weight in affix["spawn_weights"]:
+        if spawn_weight["weight"] == 0:
+            #We read the spawn_weights in order they are listed
+            #If we find one which gives matches a tag we have we in starting_tags, we ignore
+            if spawn_weight["tag"] in starting_tags:
+                break
+        elif spawn_weight["weight"] > 0:
+            pos_spawn_weight_tags.add(spawn_weight["tag"])
+        else:
+            raise ValueError("spawn_weights should be nonnegative")
+
+    return pos_spawn_weight_tags
+
+
+# TODO: MAYBE?: this should take in some generic item type and calculate the possible mods that 
+# can roll on that item. This will allow support for items which have some odd non-spawnable
+# mods on them (like league specific mod drops)
+# TODO: currently includes more tags than necessary (includes all possible relevant tags)
+def generate_all_possible_affixes_and_tags(starting_tags, mod_pool):
+    '''
+    :param starting_tags: set containing all the starting_tags on the item we are rolling
+    :param mod_pool: a dictionary containing all possible mods possible to roll on our item
+    :return: spawn_tags, affixes
+            spawn_tags -   all possible tags which can spawn from rolling this item
+            affixes -      final mod pool of all things that can spawn
+    '''
+
+    affixes = {}
+    added_new_affixes = True
+
+    while added_new_affixes:
+    
+        added_new_affixes = False
+
+        total_pos_spawn_weights_tags = set()
+
+        for key, affix in mod_pool.items():
+            # We want to only consider affixes which have a positive spawn weight for one of our potential tags
+            # We also want to keep track of which tags affect this affix, whether they show up as any spawn_weight (even 0)
+            # or they show up as a generation_weight
+
+            #important that we only block starting_tags here, as there could be routes of crafting which spawn some tags and not others which affects blocking
+            pos_spawn_weights_tags = _get_positive_spawn_weight_tags(affix, starting_tags)
+            total_pos_spawn_weights_tags = total_pos_spawn_weights_tags.union(pos_spawn_weights_tags)
+
+            spawn_weight_tags = set([spawn_weight["tag"] for spawn_weight in affix["spawn_weights"]])
+            assert pos_spawn_weights_tags.issubset(spawn_weight_tags), "pos_spawn_weight_tags should be subset of spawn_weight_tags"
+
+            add_tags = set()
+            if len(pos_spawn_weights_tags) > 0:
+                if key not in affixes:
+                    affixes[key] = affix
+                    added_new_affixes = True
+
+                add_tags = add_tags.union(set(affix["adds_tags"]))
+
+    spawn_tags = set(starting_tags.copy())
+    spawn_tags = spawn_tags.union(add_tags)    
+
+    return spawn_tags, affixes
+
+def _get_spawnable_mods_for_item(domains,ilvl):
+    '''
+    Trim by removing all affixes that can't be crafted and are in the wrong domains
+    '''
+
+    spawnable_mods = {}
+
+    for affix_key in list(mods.keys()):
+        affix_value = mods[affix_key]
+        is_prefix_or_suffix = affix_value["generation_type"] in ["prefix", "suffix"]
+        is_in_domains = affix_value["domain"] in domains
+        is_not_essence = not affix_value["is_essence_only"]
+        is_ilvl_appropriate = affix_value["required_level"] <= ilvl
+
+
+        if is_prefix_or_suffix and is_in_domains and is_not_essence and is_ilvl_appropriate:
+            spawnable_mods[affix_key] = affix_value
+    return spawnable_mods
+
+
+
+def collect_mods_and_tags(starting_tags, added_mods, domains, ilvl):
+    spawnable_mods = _get_spawnable_mods_for_item(domains, ilvl)
+    mod_pool = {**spawnable_mods, **added_mods}
+
+    #further reduce the mod pool by looking at spawn weights and tags
+    realized_spawn_tags, mod_pool = generate_all_possible_affixes_and_tags(starting_tags, mod_pool)
+
+    return {**mod_pool, **added_mods}, realized_spawn_tags
